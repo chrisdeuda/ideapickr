9b5d03b59c72f51dd1cf14294ed8f43391f52731
chrisdeuda <christopherdeuda@gmail.com>
2019-02-09 21:06:03 +0800

    feat(db): Add home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Topics;
+use App\Model\User;
+
+
+class HomeController extends Controller
+{
+    
+    public function index(){
+
+        {{ base_url  }}/{{ version  }}/venues/1/spaces/
+
+    }
+}
