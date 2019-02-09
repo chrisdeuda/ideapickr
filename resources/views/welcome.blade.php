f2cb82565a3dd37670649be19882c69accb2c68d
chrisdeuda <christopherdeuda@gmail.com>
2019-02-09 12:40:49 +0800

    Update headings of index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Writing Prompts
                 </div>
 
                 <div class="links">
