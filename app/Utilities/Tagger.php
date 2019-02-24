8fa969e2386269dea5abb308aec873a18338b33a
chrisdeuda <christopherdeuda@gmail.com>
2019-02-24 17:49:31 +0800

    feat(topic-add): Implement helper functions tagUser, getHashTag

--- app/Utilities/Tagger.php
+++ app/Utilities/Tagger.php
@@ -6,6 +6,42 @@ use App\Models\User;
 use App\Models\Topic;
 
 class Tagger{
+    /**
+     * 
+     */
+    public static function tagTopic($topic, $tags){
+        
 
+    }
+    
+    public static function tagUser($User, $Tags, $user_id){
+
+        $tags_length = count($Tags);
+
+        echo "Count". $tags_length;
+
+        for($i = 0; $i <$tags_length; $i++){
+
+            //Check if it the data doesn't exist
+            $tag = Tag::firstOrNew(
+                ['name' => $Tags[$i], 'user_id'=>$user_id ]
+            );
+            $User->tags()->save($tag);
+        }
+    }
+
+    /**
+     * It get the hashtags from the text and return an array of hastags
+     * 
+     * @param string name
+     * 
+     * @return array
+     */
+    public static function getHashTag($name){
+        $str = $name;
+        preg_match_all('/#([^\s]+)/', $str, $matches);
+        return $matches[1];
+
+    }
 
 }
